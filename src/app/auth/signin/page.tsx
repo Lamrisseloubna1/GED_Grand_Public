43bfbdcd8c76d98f10a12bc431aff12c80305e05
Lamrisseloubna1 <lamrisse.loubna245@gmail.com>
2024-07-25 10:53:08 +0100

    sign-up

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -21,9 +21,8 @@ const SignIn: React.FC = () => {
 
             <div className="w-full border-stroke dark:border-strokedark xl:w-1/1 xl:border-l-2">
               <div className="w-full p-4 sm:p-12.5 xl:p-11.5">
-                <span className="mb-1.5 block font-medium">Start for free</span>
                 <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
-                  Sign In to TailAdmin
+                  Sign In 
                 </h2>
 
                 <form>
